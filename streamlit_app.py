9a29637f4c0d644be795cac79775a6fe3e957b29
Olayinka Yusuf <67215124+Sahdam@users.noreply.github.com>
2025-12-14 09:09:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('Machine Learning: Sleep Disorders Classification')
 
 st.info('This app is a machine learning app')
+
+df = pd.read_csv("master/sleep_health_lifestyle.csv")
+df
