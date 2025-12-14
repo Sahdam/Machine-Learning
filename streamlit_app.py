02ce4af4c150604ebe80025248f74cc93625160d
Olayinka Yusuf <67215124+Sahdam@users.noreply.github.com>
2025-12-14 17:31:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,18 +19,19 @@ st.title('Machine Learning: Sleep Disorders Classification')
 
 st.info('This app is a machine learning app')
 
-with st.expander("**Data**"):
-  st.write("## Sleep, health and Lifestyle Data")
-  df = pd.read_csv("sleep_health_lifestyle.csv")
-  df.drop(columns="index", inplace=True)
-  df.set_index("Person ID", inplace=True)
-  df.fillna("None", inplace=True)
-  df
-  buffer = io.StringIO()
-  df.info(buf=buffer)
-  st.code(buffer.getvalue(), language="text")
-  st.dataframe(df.describe())
-  st.dataframe(df.select_dtypes("object").describe())
+with st.sidebar:
+  with st.expander("**Data**"):
+    st.write("## Sleep, health and Lifestyle Data")
+    df = pd.read_csv("sleep_health_lifestyle.csv")
+    df.drop(columns="index", inplace=True)
+    df.set_index("Person ID", inplace=True)
+    df.fillna("None", inplace=True)
+    df
+    buffer = io.StringIO()
+    df.info(buf=buffer)
+    st.code(buffer.getvalue(), language="text")
+    st.dataframe(df.describe())
+    st.dataframe(df.select_dtypes("object").describe())
 
 df["BMI Category"] = df["BMI Category"].replace({
     "Normal": "Normalweight",
