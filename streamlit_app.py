f77fb8fdb235acfdb8ffd4d0533b46ec9fbe462a
Olayinka Yusuf <67215124+Sahdam@users.noreply.github.com>
2025-12-14 09:12:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ st.title('Machine Learning: Sleep Disorders Classification')
 st.info('This app is a machine learning app')
 
 df = pd.read_csv("sleep_health_lifestyle.csv")
+df.drop(columns="index", inplace=True)
+df.set_index("Person ID", inplace=True)
+df.fillna("None", inplace=True)
 df
