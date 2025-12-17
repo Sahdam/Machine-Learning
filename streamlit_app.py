85bfa9aa6060d2ab300e9b8cf74df0408352f4ce
Olayinka Yusuf <67215124+Sahdam@users.noreply.github.com>
2025-12-17 17:12:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,28 +31,26 @@ if "show_data" not in st.session_state:
 
 with st.sidebar.container():
     d_btn = st.button("**Data**", key="d_btn")
+df = pd.read_csv("sleep_health_lifestyle.csv")
+df.drop(columns="index", inplace=True)
+df.set_index("Person ID", inplace=True)
+df.fillna("None", inplace=True)
 if d_btn:
     grid1.write("## Sleep, health and Lifestyle Data")
-    df = pd.read_csv("sleep_health_lifestyle.csv")
-    df.drop(columns="index", inplace=True)
-    df.set_index("Person ID", inplace=True)
-    df.fillna("None", inplace=True)
     data_btn = grid1.button("**Show Data**", key="data_btn")
     data_reset =grid1.button("**Show no data**", key="data_reset")
-    if data_btn:
-      st.session_state.show_data = True
-    if data_reset:
-      st.session_state.show_data = False
-    if st.session_state.show_data:
-      grid1.dataframe(df)
-      buffer = io.StringIO()
-      df.info(buf=buffer)
-      grid1.code(buffer.getvalue(), language="text")
-      grid1.dataframe(df.describe())
-      grid1.dataframe(df.select_dtypes("object").describe())
-      df["BMI Category"] = df["BMI Category"].replace({
-            "Normal": "Normalweight",
-            "Normal Weight": "Normalweight"})
+if data_btn:
+    st.session_state.show_data = True
+if data_reset:
+    st.session_state.show_data = False
+if st.session_state.show_data:
+    grid1.dataframe(df)
+    buffer = io.StringIO()
+    df.info(buf=buffer)
+    grid1.code(buffer.getvalue(), language="text")
+    grid1.dataframe(df.describe())
+    grid1.dataframe(df.select_dtypes("object").describe())
+df["BMI Category"] = df["BMI Category"].replace({"Normal": "Normalweight","Normal Weight": "Normalweight"})
 
 grid2 = grid(1,[4, 2, 1], 1, vertical_align="top")
 if "show_plot" not in st.session_state:
