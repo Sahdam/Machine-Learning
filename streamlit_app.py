b001fdd50e2a5c18196237a345d060d095fb6cc9
Olayinka Yusuf <67215124+Sahdam@users.noreply.github.com>
2025-12-18 01:15:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -348,11 +348,10 @@ def prediction(gender, age, occupation, sleep_duration,sleep_quality,physical_ac
     blood_pre = st.text_input("enter blood pressure", max_chars=30)
     hr = st.number_input("enter heart rate", min_value = 10, max_value=100)
     daily_step = st.number_input("enter daily steps", min_value = 1, max_value=10000, step=100)
-    data =pd.DataFrame({
+    data = pd.DataFrame({
         'Gender': [gender], 'Age': [age], 'Occupation': [occupation], 'Sleep Duration': [sleep_duration],'Quality of Sleep': [sleep_quality],
        'Physical Activity Level':[physical_act, 'Stress Level':[stress_level], 'BMI Category': [bmi],
-       'Blood Pressure': [blood_pre], 'Heart Rate': [hr], 'Daily Steps': [daily_step]
-    })
+       'Blood Pressure': [blood_pre], 'Heart Rate': [hr], 'Daily Steps': [daily_step]})
     model_pred_name = st.selectbox(
         "Choose model",
         ["Logistic Regression", "Decision Tree", "Random Forest", "Gradient Boosting"],
