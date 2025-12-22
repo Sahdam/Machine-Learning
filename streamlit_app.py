af02b41bd7dc52a6efb5c3ec137de4e10b180229
Olayinka Yusuf <67215124+Sahdam@users.noreply.github.com>
2025-12-22 22:18:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ for m in ["model_lr", "model_dt", "model_rf", "model_gb"]:
     st.session_state.setdefault(m, None)
 
 # SIDEBAR NAVIGATION
-with st.topbar:
+with st.top:
     st.title("Navigation")
     st.session_state.page = st.radio(
         " ",[
